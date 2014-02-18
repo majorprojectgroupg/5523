3470aca9048779a93aa0159be3675b49bd160d80
jhyphend <jhyphend@gmail.com>
2014-02-18 14:53:31 +0000

    Update Parser.java
    Parser is now an abstract class, AndroidParser and AdminParser are its subclasses.

--- SurveyAdminApp/src/data/Parser.java
+++ SurveyAdminApp/src/data/Parser.java
@@ -14,69 +14,70 @@ import org.json.JSONTokener;
  *   "questions" : JSONArray 
  * }
  */
-public class Parser {
+public abstract class Parser {
 
-	JSONObject data;
-	JSONArray questions;
+	protected JSONObject data; 			// this is the whole questionnaire
+	protected JSONArray questions;		// this is just the array of questions, without patient data
 	
 	public Parser() {
 		data = new JSONObject();
 	}
 	
-	public Parser(String datafeed) throws JSONException {
-		data = (JSONObject) new JSONTokener(datafeed).nextValue();
+	public Parser(String datafeed) {
+		
+		try {
+			data = (JSONObject) new JSONTokener(datafeed).nextValue();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	// parses the questionnaire into a usable format
-	public void setData(String datafeed) throws JSONException {
-		data = (JSONObject) new JSONTokener(datafeed).nextValue();
+	public void setData(String datafeed) {
+		try {
+			data = (JSONObject) new JSONTokener(datafeed).nextValue();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 		
-	public String getPatientFirstname() throws JSONException {
-		return data.getString("firstname");
-	}
-	
-	public String getPatientLastname() throws JSONException {
-		return data.getString("lastname");
-	}
-	
-	public String getPatientBirthdate() throws JSONException {
-		return data.getString("birthdate");
-	}
-	
-	// iterates over the JSONArray of questions and extracts the question information (INCOMPLETE)
-	public void getQuestions() throws JSONException {
-		questions = data.getJSONArray("questions");
-		
-		for (int i = 0; i < questions.length(); i++) {
-			
-			// still requires code to extract the data from each JSONObject and populate the
-			// UI's questionnaire
-			if (((JSONObject) questions.get(i)).get("qType").equals("text")) {}
-			else if (((JSONObject) questions.get(i)).get("qType").equals("number")) {}
-			else if (((JSONObject) questions.get(i)).get("qType").equals("multiple")) {}
-			else if (((JSONObject) questions.get(i)).get("qType").equals("date")) {} 
+	public String getPatientFirstname() {
+		try {
+			return data.getString("firstname");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "Error: JSONException";
 		}
 	}
 	
-	// once the patient's completed the questionnaire, this pulls the answers from the questionnaire
-	// widgets and adds them to the questionnaire object
-	public void setAnswers() {				
-				
-		for (int i = 0; i < questions.length(); i++) {
-			
-			// declare a local variable, assign it using the corresponding question's widget from
-			// the questionnaire; still needs to be implemented
-			
-			// add the answer to the corresponding question (VALUE is a placeholder, update this when
-			// the UI has been built
-			((JSONObject) questions.get(i)).put("answer", VALUE);
+	public String getPatientLastname() {
+		try {
+			return data.getString("lastname");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "Error: JSONException";
 		}
-		
 	}
 	
-	// now that the answers have been added to the questionnaire, the data can be sent back to the
-	// survey administration app
+	public String getPatientBirthdate() {
+		try {
+			return data.getString("birthdate");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "Error: JSONException";
+		}
+	}
+	
+	// iterates over the JSONArray of questions and extracts the question information
+	// defined by subclass
+	public abstract void getQuestions();		
+	
+	// for sending the question data in the form of a String
 	public String getData() {
 		return data.toString(); 
 	}
